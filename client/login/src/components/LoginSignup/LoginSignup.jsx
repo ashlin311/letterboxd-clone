8e415d1907225d2cfff2dcecc54b0af5b8a6c2e1
AbishekPrabeesh <150554171+AbishekPrabeesh@users.noreply.github.com>
2025-08-08 15:30:09 +0530

    password masking

--- client/login/src/components/LoginSignup/LoginSignup.jsx
+++ client/login/src/components/LoginSignup/LoginSignup.jsx
@@ -1,25 +1,50 @@
-import React from 'react'
-import './LoginSignup.css'
-import user_icon from '../assets/person.png'
-import email_icon from '../assets/email.png'
-import password_icon from '../assets/password.png'
+import React, { useState } from 'react';
+import './LoginSignup.css';
+import user_icon from '../assets/person.png';
+import email_icon from '../assets/email.png';
+import password_icon from '../assets/password.png';
 
 const LoginSignup = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
     <div className="container">
       <div className="text">Sign Up</div>
       <div className="inputs">
         <div className="input">
-          <img src={user_icon} alt="name"/>
-          <input type="text" placeholder='Username' />
+          <img src={user_icon} alt="name" />
+          <input type="text" placeholder="Username" />
         </div>
         <div className="input">
-          <img src={email_icon} alt="mail"/>
-          <input type="email" placeholder='Email ID'/>
+          <img src={email_icon} alt="mail" />
+          <input type="email" placeholder="Email ID" />
         </div>
-        <div className="input">
-          <img src={password_icon} alt="password"/>
-          <input type="password" placeholder='Password' />
+        <div className="input" style={{ position: 'relative' }}>
+          <img src={password_icon} alt="password" />
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            style={{ paddingRight: '50px' }}
+          />
+          <span
+            onClick={togglePassword}
+            style={{
+              position: 'absolute',
+              right: '10px',
+              top: '50%',
+              transform: 'translateY(-50%)',
+              cursor: 'pointer',
+              fontSize: '14px',
+              color: '#888',
+              userSelect: 'none'
+            }}
+          >
+            {showPassword ? 'Hide' : 'Show'}
+          </span>
         </div>
       </div>
       <div className="forgot-password">Forgot Password? <span>Click Here</span></div>
@@ -28,5 +53,4 @@ const LoginSignup = () => {
     </div>
   );
 };
-
-export default LoginSignup
+export default LoginSignup;
