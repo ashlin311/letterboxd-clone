7ea88f820934002e89113405a03f0e506f40eb93
ashlin311 <103652855+ashlin311@users.noreply.github.com>
2025-09-25 19:24:33 +0530

    NowShowing update

---
+++ .history/server/lib/movieNowShowing_20250924214111.js
@@ -0,0 +1,9 @@
+import supabase from '../supabase.js';
+
+async function getNowShowingMovies() {
+    const { data, error } = await supabase
+    .from('movies')
+    .update({ now_showing: true })
+    .gte('release_date', '2025-08-01')
+    
+}
---
+++ .history/server/lib/movieNowShowing_20250924214128.js
@@ -0,0 +1,10 @@
+import supabase from '../supabase.js';
+
+async function getNowShowingMovies() {
+    const { data, error } = await supabase
+    .from('movies')
+    .update({ now_showing: true })
+    .gte('release_date', '2025-08-01')
+    .lte('release_date', '2025-09-31')
+    .select
+}
