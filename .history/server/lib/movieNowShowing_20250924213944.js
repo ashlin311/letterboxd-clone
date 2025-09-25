cbe182277b1250a4a1c9f978da9ab2cb1a6c910f
ashlin311 <103652855+ashlin311@users.noreply.github.com>
2025-09-25 19:37:44 +0530

    Revert "NowShowing update"
    This reverts commit 74a13a9c2ad0265f3e1ae5177578af7983fa160b.

--- .history/server/lib/movieNowShowing_20250924213944.js
+++
@@ -1,5 +0,0 @@
-import supabase from '../supabase.js';
-
-async function getNowShowingMovies() {
-    const { data, error } = await supabase
-}
--- .history/server/lib/movieNowShowing_20250924214005.js
+++
@@ -1,8 +0,0 @@
-import supabase from '../supabase.js';
-
-async function getNowShowingMovies() {
-    const { data, error } = await supabase
-    .from('movies')
-    .update({ now_showing: true })
-    .gte()
-}
