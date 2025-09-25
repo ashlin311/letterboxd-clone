b8487f9a23f17c8d1e290757ba8693342ee62392
ashlin311 <103652855+ashlin311@users.noreply.github.com>
2025-09-25 20:13:39 +0530

    This reverts commit 74a13a9c2ad0265f3e1ae5177578af7983fa160b.

--- .history/server/lib/movieNowShowing_20250924214111.js
+++
@@ -1,9 +0,0 @@
-import supabase from '../supabase.js';
-
-async function getNowShowingMovies() {
-    const { data, error } = await supabase
-    .from('movies')
-    .update({ now_showing: true })
-    .gte('release_date', '2025-08-01')
-    
-}
